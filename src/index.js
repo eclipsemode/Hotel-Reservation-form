196e580824fac17608150526ec64f9991602ef86
Daniel Volkonesko <eclipsemod555@gmail.com>
2021-11-22 20:15:05 +0300

    dropdown-guest-number

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import AirDatepicker from 'air-datepicker';
 import 'air-datepicker/air-datepicker.css';
 import "./style/style.scss";
 import ArrowSvg from './style/assets/images/arrow_forward.svg';
+import calendar from './style/js/calendar';
+import dropdown from './style/js/dropdown';
+// import { render } from 'pug';
 
 
 
@@ -15,6 +18,8 @@ import ArrowSvg from './style/assets/images/arrow_forward.svg';
 //         dp.setViewDate(date);
 //     },
 // }
+
+
 // new AirDatepicker('#el', {
 //     buttons: ['clear', selectItem],
 //     multipleDatesSeparator: ' - ',
@@ -42,104 +47,3 @@ import ArrowSvg from './style/assets/images/arrow_forward.svg';
 //     range: true,
 // })
 
-// CLICK ON BUTTON DROPDOWN. OPEN / CLOSE THE SELECT / ITEM SELECTION FROM LIST. MEMORISE SELECTED ITEM. CLOSE DROPDOWN
-document.addEventListener('click', (event) => {
-    if (event.target.closest('.dropdown-type')) {
-        document.querySelector('.dropdown').classList.toggle('active');
-    } else if (!event.target.closest('.dropdown')) {
-        document.querySelector('.dropdown').classList.remove('active');
-    } else if (event.target.closest('.dropdown-menu__list-item')) {
-        document.querySelectorAll('.dropdown-menu__list-item').forEach(function (listItem) {
-            listItem.addEventListener('click', function () {
-                document.querySelector('.dropdown-type').innerText = this.innerText;
-                document.querySelector('.dropdown-type').focus();
-                document.querySelector('.dropdown').classList.remove('active');
-            })
-        });
-    }
-});
-
-// ITEM COUNTER
-const buttons = document.querySelectorAll(".counter__btn");
-buttons.forEach(btn => {
-    btn.addEventListener("click", function () {
-        const direction = this.dataset.direction;
-
-        const inp = this.parentElement.querySelector(".counter__value");
-
-        const currentValue = +inp.value;
-
-        let newValue;
-
-        if (direction === "plus") {
-            newValue = currentValue + 1;
-        } else {
-            newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
-        }
-
-        inp.value = newValue;
-    })
-})
-
-
-const date = new Date();
-
-date.setDate(1);
-
-const monthDays = document.querySelector('.calendar_days');
-
-const lastDay = new Date(date.getFullYear(),
-    date.getMonth() + 1, 0).getDate();
-
-const prevLastDay = new Date(date.getFullYear(),
-    date.getMonth(), 0).getDate();
-
-const firstDayIndex = date.getDate() - 1;
-
-const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1, 0).getDay();
-
-const nextDays = 7 - lastDayIndex;
-
-const months = [
-    "Январь",
-    "Февраль",
-    "Март",
-    "Апрель",
-    "Май",
-    "Июнь",
-    "Июль",
-    "Август",
-    "Сентябрь",
-    "Октябрь",
-    "Ноябрь",
-    "Декабрь"
-];
-
-document.querySelector('.calendar_month__date h2').innerHTML = months[date.getMonth()];
-
-document.querySelector('.calendar_month__date span').innerHTML = date.getFullYear();
-
-let days = "";
-
-for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="calendar_days__prev-date "><p>${prevLastDay - x + 1}</p></div>`;
-}
-
-for (let i = 1; i <= lastDay; i++) {
-    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-        days += `<div class="calendar_days__today"><p>${i}</p></div>`;
-    } else {
-        days += `<div><p>${i}</p></div>`;
-    }
-}
-
-for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="calendar_days__next-date"><p>${j}</p></div>`;
-    monthDays.innerHTML = days;
-}
-
-document.querySelector(".calendar_month__left").addEventListener('click', () => {
-
-})
---
+++ src/style/js/dropdown.js
@@ -0,0 +1,75 @@
+// CLICK ON BUTTON DROPDOWN. OPEN /  CLOSE DROPDOWN
+document.addEventListener('click', (event) => {
+    if (event.target.closest('.dropdown-type')) {
+        document.querySelector('.dropdown').classList.toggle('active');
+    } else if (!event.target.closest('.dropdown')) {
+        document.querySelector('.dropdown').classList.remove('active');
+    }
+});
+
+// ITEM COUNTER
+
+const buttons = document.querySelectorAll(".counter__btn");
+const sumArr = [+document.querySelector('[name="adults"]').value, +document.querySelector('[name="children"]').value, +document.querySelector('[name="babies"]').value];;
+
+buttons.forEach(btn => {
+    btn.addEventListener("click", function (event) {
+        if (event.target.closest('.dropdown')) {
+            document.querySelector('.dropdown-type').focus();
+        }
+        const direction = this.dataset.direction;
+
+        const inp = this.parentElement.querySelector(".counter__value");
+
+        const currentValue = +inp.value;
+
+        let newValue;
+
+        if (direction === "plus") {
+            newValue = currentValue + 1;
+        } else {
+            newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+        }
+
+        inp.value = newValue;
+
+        if (inp.value > 0) {
+            inp.previousElementSibling.style = 'opacity: 1;';
+        } else {
+            inp.previousElementSibling.style = 'opacity: 0.5;';
+        }
+
+        if (inp.value >= 9) {
+            inp.value = 9;
+            inp.nextElementSibling.style = 'opacity: 0.5;';
+        } else {
+            inp.nextElementSibling.style = 'opacity: 1;';
+        }
+    });
+});
+
+// DROPDOWN BUTTONS CLEAR AND SUBMIT
+
+document.querySelectorAll('.dropdown-guest__list-button').forEach(element => {
+    let dropText = document.querySelector('.dropdown-type').textContent;
+    element.addEventListener('click', (event) => {
+        if (event.target.closest('[name="submit"]')) {
+            let countGuest = +document.querySelector('[name="children"]').value + +document.querySelector('[name="adults"]').value + +document.querySelector('[name="babies"]').value;
+            document.querySelector('.dropdown-type').textContent = countGuest;
+            document.querySelector('.dropdown').classList.remove('active');
+            document.querySelector('.dropdown-type').focus();
+        } else if (event.target.closest('[name="clear"]')) {
+            document.querySelector('.dropdown-type').textContent = dropText;
+        }
+
+        if (document.querySelector('.dropdown-type').textContent == 0) {
+            document.querySelector('.dropdown-type').textContent = dropText;
+        } else if (document.querySelector('.dropdown-type').textContent == 1) {
+            document.querySelector('.dropdown-type').textContent += ' Гость';
+        } else if (document.querySelector('.dropdown-type').textContent > 1 && document.querySelector('.dropdown-type').textContent < 5) {
+            document.querySelector('.dropdown-type').textContent += ' Гостя';
+        } else if (document.querySelector('.dropdown-type').textContent > 4) {
+            document.querySelector('.dropdown-type').textContent += ' Гостей';
+        }
+    })
+});
