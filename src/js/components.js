31cd67a1fe85e2367bf3b9a44e9547347a94bdad
Daniel Volkonesko <eclipsemod555@gmail.com>
2021-12-07 17:58:48 +0300

    refactoring

--- src/js/components.js
+++ src/js/components.js
@@ -1,4 +1,4 @@
-import calendar from '../js/calendar';
+import calendar from '../components/calendar/calendar';
 import dropdown from '../components/dropdown/dropdown';
 import radioButton from '../components/radio-buttons/radio-buttons';
 import expandableCheckbox from '../components/expandable-checkbox-list/expandable-checkbox-list';
