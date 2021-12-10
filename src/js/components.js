e93682831620c5fed9528385786ed27ceb1b0cdb
Daniel Volkonesko <eclipsemod555@gmail.com>
2021-12-10 17:12:51 +0300

    rate-button

--- src/js/components.js
+++ src/js/components.js
@@ -3,3 +3,4 @@ import dropdown from '../components/dropdown/dropdown';
 import radioButton from '../components/radio-buttons/radio-buttons';
 import expandableCheckbox from '../components/expandable-checkbox-list/expandable-checkbox-list';
 import likeButton from '../components/like-button/like-button';
+
