4a41ed81efa333bbd0b7134572576efd93a5ff24
Daniel Volkonesko <eclipsemod555@gmail.com>
2021-12-22 13:03:08 +0300

    refactoring

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -1,29 +1,38 @@
+const dropdown = document.querySelector('.dropdown');
+const dropdownButton = document.querySelector('.dropdown-type');
+const babiesSelector = document.querySelector('[name="babies"]');
+const childrenSelector = document.querySelector('[name="children"]');
+const adultsSelector = document.querySelector('[name="adults"]');
+const clearButton = document.querySelector('[name="clear"]');
+
 // CLICK ON BUTTON DROPDOWN. OPEN /  CLOSE DROPDOWN
 document.addEventListener('click', (event) => {
     if (event.target.closest('.dropdown-type')) {
-        document.querySelector('.dropdown').classList.toggle('active');
+        dropdown.classList.toggle('active');
     } else if (!event.target.closest('.dropdown')) {
-        document.querySelector('.dropdown').classList.remove('active');
-    } else if (document.querySelector('.dropdown').classList.contains('active')) {
-        document.querySelector('.dropdown-type').focus();
+        dropdown.classList.remove('active');
+    } else if (dropdown.classList.contains('active')) {
+        dropdownButton.focus();
     }
 
-    if (document.querySelector('[name="children"]').value == 0 && document.querySelector('[name="adults"]').value == 0 && document.querySelector('[name="babies"]').value == 0) {
-        document.querySelector('[name="clear"]').style = 'opacity: 0; pointer-events: none;';
+    if (childrenSelector.value == 0 && adultsSelector.value == 0 && babiesSelector.value == 0) {
+        clearButton.style = 'opacity: 0; pointer-events: none;';
     } else {
-        document.querySelector('[name="clear"]').style = 'opacity: 1; pointer-events: auto;';
-        document.querySelector('[name="clear"]').setAttribute('style', ':hover {opacity: 0.5;}')
+        clearButton.style = 'opacity: 1; pointer-events: auto;';
+        clearButton.setAttribute('style', ':hover {opacity: 0.5;}')
     }
 });
 
 // ITEM COUNTER
-
 const buttons = document.querySelectorAll(".counter__btn");
-const sumArr = [+document.querySelector('[name="adults"]').value, +document.querySelector('[name="children"]').value, +document.querySelector('[name="babies"]').value];
+const sumArr = [+adultsSelector.value, +childrenSelector.value, +babiesSelector.value];
+
+// document.getElementsByClassName('counter__value').forEach(element => {
+//     console.log('dsds')
+// })
 
 buttons.forEach(btn => {
     btn.addEventListener("click", function (event) {
-
         const inp = this.parentElement.querySelector(".counter__value");
         const currentValue = +inp.value;
         let newValue;
@@ -49,39 +58,41 @@ buttons.forEach(btn => {
             inp.nextElementSibling.style = 'opacity: 1;';
         }
     });
+
+
 });
 
 // DROPDOWN BUTTONS CLEAR AND SUBMIT
-
 document.querySelectorAll('.dropdown-guest__list-button').forEach(element => {
-    let dropText = document.querySelector('.dropdown-type').textContent;
+    let dropText = dropdownButton.textContent;
     element.addEventListener('click', (event) => {
         if (event.target.closest('[name="submit"]')) {
-            let countGuest = +document.querySelector('[name="children"]').value + +document.querySelector('[name="adults"]').value + +document.querySelector('[name="babies"]').value;
-            document.querySelector('.dropdown-type').textContent = countGuest;
-            document.querySelector('.dropdown').classList.remove('active');
-            document.querySelector('.dropdown-type').focus();
+            let countGuest = +childrenSelector.value + +adultsSelector.value + +babiesSelector.value;
+            dropdownButton.textContent = countGuest;
+            dropdown.classList.remove('active');
+            dropdownButton.focus();
         } else if (event.target.closest('[name="clear"]')) {
-            document.querySelector('.dropdown-type').textContent = dropText;
-            document.querySelector('[name="children"]').value = 0;
-            document.querySelector('[name="children"]').previousElementSibling.style = 'opacity: 0.5;';
-            document.querySelector('[name="children"]').nextElementSibling.style = 'opacity: 1;';
-            document.querySelector('[name="adults"]').value = 0;
-            document.querySelector('[name="adults"]').previousElementSibling.style = 'opacity: 0.5;';
-            document.querySelector('[name="adults"]').nextElementSibling.style = 'opacity: 1;';
-            document.querySelector('[name="babies"]').value = 0;
-            document.querySelector('[name="babies"]').previousElementSibling.style = 'opacity: 0.5;';
-            document.querySelector('[name="babies"]').nextElementSibling.style = 'opacity: 1';
+            dropdownButton.textContent = dropText;
+            childrenSelector.value = 0;
+            childrenSelector.previousElementSibling.style = 'opacity: 0.5;';
+            childrenSelector.nextElementSibling.style = 'opacity: 1;';
+            adultsSelector.value = 0;
+            adultsSelector.previousElementSibling.style = 'opacity: 0.5;';
+            adultsSelector.nextElementSibling.style = 'opacity: 1;';
+            babiesSelector.value = 0;
+            babiesSelector.previousElementSibling.style = 'opacity: 0.5;';
+            babiesSelector.nextElementSibling.style = 'opacity: 1';
         }
 
-        if (document.querySelector('.dropdown-type').textContent == 0) {
-            document.querySelector('.dropdown-type').textContent = dropText;
-        } else if (document.querySelector('.dropdown-type').textContent != 11 && document.querySelector('.dropdown-type').textContent.slice(-1) == 1) {
-            document.querySelector('.dropdown-type').textContent += ' гость';
-        } else if (document.querySelector('.dropdown-type').textContent > 1 && document.querySelector('.dropdown-type').textContent < 5 || document.querySelector('.dropdown-type').textContent > 20 && document.querySelector('.dropdown-type').textContent.slice(-1) < 5) {
-            document.querySelector('.dropdown-type').textContent += ' гостя';
-        } else if (document.querySelector('.dropdown-type').textContent !== dropText) {
-            document.querySelector('.dropdown-type').textContent += ' гостей';
+        // Guest declination
+        if (dropdownButton.textContent == 0) {
+            dropdownButton.textContent = dropText;
+        } else if (dropdownButton.textContent != 11 && dropdownButton.textContent.slice(-1) == 1) {
+            dropdownButton.textContent += ' гость';
+        } else if (dropdownButton.textContent > 1 && dropdownButton.textContent < 5 || dropdownButton.textContent > 20 && dropdownButton.textContent.slice(-1) < 5) {
+            dropdownButton.textContent += ' гостя';
+        } else if (dropdownButton.textContent !== dropText) {
+            dropdownButton.textContent += ' гостей';
         }
     })
 });
