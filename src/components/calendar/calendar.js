2a9b8a426a43c8c2ce95afc7be8a3ac409cab04b
Daniel Volkonesko <eclipsemod555@gmail.com>
2021-12-26 19:28:39 +0300

    airdatepicker

--- src/components/calendar/calendar.js
+++ src/components/calendar/calendar.js
@@ -1,91 +1,24 @@
-const date = new Date();
+import AirDatepicker from 'air-datepicker';
+import 'air-datepicker/air-datepicker.css';
 
-const renderCalendar = () => {
-    date.setDate(7);
+// AIR DATEPICKER
+let selectItem = {
+    content: 'Применить',
+    className: 'custom-button-classname',
+    onClick: (dp) => {
+        let date = new onSelect();
+        dp.selectDate(date);
+        dp.setViewDate(date);
+    },
+}
 
-    const monthDays = document.querySelector('.calendar_days');
-
-    const lastDay = new Date(
-        date.getFullYear(),
-        date.getMonth() + 1,
-        0
-    ).getDate();
-
-    const prevLastDay = new Date(
-        date.getFullYear(),
-        date.getMonth(),
-        0
-    ).getDate();
-
-    const firstDayIndex = date.getDay();
-
-    const lastDayIndex = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
-
-    const nextDays = 7 - lastDayIndex;
-
-    const months = [
-        "Январь",
-        "Февраль",
-        "Март",
-        "Апрель",
-        "Май",
-        "Июнь",
-        "Июль",
-        "Август",
-        "Сентябрь",
-        "Октябрь",
-        "Ноябрь",
-        "Декабрь"
-    ];
-
-    document.querySelector('.calendar_month__date h2').innerHTML = months[date.getMonth()];
-
-    document.querySelector('.calendar_month__date span').innerHTML = date.getFullYear();
-
-    let days = "";
-
-    for (let x = firstDayIndex; x > 0; x--) {
-        days += `<div class="calendar_days__prev-date ">${prevLastDay - x + 1}</div>`;
-    }
-
-    for (let i = 1; i <= lastDay; i++) {
-        if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
-            days += `<div class="calendar_days__today">${i}</div>`;
-        } else {
-            days += `<div>${i}</div>`;
-        }
-    }
-
-    for (let j = 1; j <= nextDays; j++) {
-        days += `<div class="calendar_days__next-date">${j}</div>`;
-        monthDays.innerHTML = days;
-    }
-
-
-    monthDays.addEventListener('click', (e) => {
-        e.currentTarget.childNodes.forEach((x) => {
-            x.classList.remove("selected--day")
-        })
-        e.target.classList.add("selected--day")
-    });
-
-    document.querySelector('.calendar_buttons__clear').addEventListener('click', () => {
-        monthDays.childNodes.forEach((i) => {
-            i.classList.remove('selected--day');
-        })
-    })
-
-};
-
-document.querySelector(".calendar_month__left").addEventListener('click', () => {
-    date.setMonth(date.getMonth() - 1);
-    renderCalendar();
-});
-
-document.querySelector(".calendar_month__right").addEventListener('click', () => {
-    date.setMonth(date.getMonth() + 1);
-    renderCalendar();
-});
-
-
-renderCalendar();
+new AirDatepicker('#el', {
+    buttons: ['clear', selectItem],
+    multipleDatesSeparator: ' - ',
+    minDate: new Date(),
+    multipleDates: 2,
+    range: true,
+    navTitles: {
+        days: 'MMMM <i>yyyy</i>',
+    },
+})
--- src/index.js
+++ src/index.js
@@ -1,52 +1,6 @@
-// import AirDatepicker from 'air-datepicker';
-// import 'air-datepicker/air-datepicker.css';
-
-import "./style/style.scss";
 import components from './js/components';
+import "./style/style.scss";
 import $ from 'jquery';
 import React from 'react';
 import ReactDom from "react-dom";
-
 // import { render } from 'pug';
-
-
-
-// // AIR DATEPICKER
-// let selectItem = {
-//     content: 'Применить',
-//     className: 'custom-button-classname',
-//     onClick: (dp) => {
-//         let date = new onSelect();
-//         dp.selectDate(date);
-//         dp.setViewDate(date);
-//     },
-// }
-
-
-// new AirDatepicker('#el', {
-//     buttons: ['clear', selectItem],
-//     multipleDatesSeparator: ' - ',
-//     classes: 'dp-style',
-//     minDate: new Date(),
-// })
-// new AirDatepicker('#el2', {
-//     buttons: ['clear', selectItem],
-//     multipleDatesSeparator: ' - ',
-//     classes: 'dp-style',
-//     minDate: new Date(),
-// })
-// new AirDatepicker('#el3', {
-//     buttons: ['clear', selectItem],
-//     multipleDatesSeparator: ' - ',
-//     classes: 'dp-style',
-//     minDate: new Date(),
-// })
-// new AirDatepicker('#el-filter', {
-//     buttons: ['clear', selectItem],
-//     multipleDatesSeparator: ' - ',
-//     classes: 'dp-style',
-//     minDate: new Date(),
-//     multipleDates: 2,
-//     range: true,
-// })
-
