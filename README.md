1ac92dd8d23c9762238610233ad81c9a884c351f
Daniel Volkonesko <eclipsemod555@gmail.com>
2022-02-23 00:50:46 +0300

    clearing

--- README.md
+++
@@ -1,2 +0,0 @@
-# Hotel-Reservation-form
-MetaLamp 2Step project
--- src/components/like-button/like-button.js
+++
@@ -1,7 +0,0 @@
-const likeBtn = document.querySelectorAll(".like-btn");
-
-likeBtn.forEach(element => {
-    element.addEventListener("click", () => {
-        element.classList.toggle("like-btn--active");
-    });
-});
